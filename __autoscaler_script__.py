3e83a2912d92ddcd83fa341797b68e7218dcfbe0
LIHAOJIA <hxl735@case.edu>
2025-01-16 15:04:17 -0500

    add funcrion run nnunet clearml

---
+++ __autoscaler_script__.py
@@ -0,0 +1,72 @@
+import argparse
+import os
+import sys
+import boto3
+
+from pathlib import Path
+
+from query_raven_image import curate_input_image
+from run_nnunet import download_nnunet_model, nnUNet_predict
+import glob
+import pdb
+# initialize s3 client
+s3_client = boto3.client("s3")
+
+
+def upload_output_folder_to_s3(output_folder, s3_uri):
+    """
+    Uploads the entire contents of the specified output folder to the given S3 URI.
+
+    Args:
+        output_folder (str): The local output folder to upload.
+        s3_uri (str): The S3 URI to upload the folder to, e.g., 's3://bucket/path/'.
+    """
+    # Extract bucket and prefix (key) from S3 URI
+    s3_parts = s3_uri.replace("s3://", "").split("/", 1)
+    bucket = s3_parts[0]
+    prefix = s3_parts[1] if len(s3_parts) > 1 else ""
+
+    # Iterate over all files and subdirectories in the output folder recursively
+    for root, _, files in os.walk(output_folder):
+        for file_name in files:
+            # Construct the local file path
+            local_path = os.path.join(root, file_name)
+
+            # Construct the S3 key by preserving the folder structure
+            relative_path = os.path.relpath(local_path, output_folder)
+            s3_key = os.path.join(prefix, relative_path)
+
+            # Upload the file to S3
+            print(f"Uploading {local_path} to s3://{bucket}/{s3_key}")
+            s3_client.upload_file(local_path, bucket, s3_key)
+            print(f"Uploaded {file_name} successfully.")
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="nnunet Lung Lesion Segmentation AutoScaler Pipeline"
+    )
+    parser.add_argument(
+        "--series-uid", required=True, type=str, help="uid of the series to be processed"
+    )
+    parser.add_argument(
+        "--s3-output-uri",
+        # default="s3://cml-storage/rushil/test_outputs/",
+        type=str,
+        help="Output directory in S3.",
+    )
+
+
+    _args = parser.parse_args()
+    image_save_path = "images/"
+    curate_input_image(_args.series_uid, image_save_path)
+    # pdb.set_trace()
+    download_nnunet_model()
+    input_image_path = glob.glob(f'{image_save_path}cropped-image-volumes/*nii.gz')[0]
+    output_path = os.path.basename(input_image_path).replace('.nii.gz', '')
+    os.makedirs(output_path, exist_ok=True)
+    nnUNet_predict(input_image_path, output_path)
+
+    upload_output_folder_to_s3(output_path, _args.s3_output_uri)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ query_raven_image.py
@@ -0,0 +1,26 @@
+import raven as rv
+import SimpleITK as sitk
+import os
+from crop_volumes import run_lungmask_crop_on_dataset
+from pathlib import Path
+
+
+def curate_input_image(series_uid, image_save_path):
+  os.makedirs(f"{image_save_path}", exist_ok=True)
+  os.makedirs(f"{image_save_path}lungmask-volumes", exist_ok=True)
+  os.makedirs(f"{image_save_path}image-volumes", exist_ok=True)
+
+  image = rv.get_images(series_uid = series_uid)[0]
+  mask = rv.get_masks(series_uid = series_uid)[0]
+
+  mask_sitk = rv.as_sitk(mask)
+  image_sitk = rv.as_sitk(image)
+
+  nifti_filepath_mask = f"{image_save_path}lungmask-volumes/{image.dataset_id}__{image.clinical_id}__{series_uid}.nii.gz"
+  nifti_filepath_image = f"{image_save_path}image-volumes/{image.dataset_id}__{image.clinical_id}__{series_uid}.nii.gz"
+  # Save the image as a NIfTI file
+  sitk.WriteImage(mask_sitk, nifti_filepath_mask)
+  sitk.WriteImage(image_sitk, nifti_filepath_image)
+
+  n_jobs = 1
+  run_lungmask_crop_on_dataset(Path(image_save_path), n_jobs=n_jobs)
